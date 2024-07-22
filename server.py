15f410e1e085656628344c41480ee453b95be126
squirrelnest <mikkasoft@gmail.com>
2024-07-22 19:10:48 -0400

    fix linter errors

--- server.py
+++ server.py
@@ -1,25 +1,29 @@
+"""Provides server functionality"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    """Serves home page"""
     return render_template('index.html')
 
 @app.route('/emotionDetector', methods=['GET'])
 def analyze():
-    input = request.args.get("textToAnalyze")
-    data = emotion_detector(input)
+    """Defines API endpoint for text analysis"""
+    text = request.args.get("textToAnalyze")
+    data = emotion_detector(text)
     if data["dominant_emotion"] == "None":
         return "<b>Invalid text! Please try again!</b>"
-    else:
-        return f"For the given statement, the system response is \
-        'anger': {data['anger']}, \
-        'disgust': {data['disgust']}, \
-        'fear': {data['fear']}, \
-        'joy': {data['joy']} and \
-        'sadness': {data['sadness']}. \
-        The dominant emotion is {data['dominant_emotion']}."
+
+    return f"For the given statement, the system response is \
+    'anger': {data['anger']}, \
+    'disgust': {data['disgust']}, \
+    'fear': {data['fear']}, \
+    'joy': {data['joy']} and \
+    'sadness': {data['sadness']}. \
+    The dominant emotion is {data['dominant_emotion']}."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
