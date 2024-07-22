8c3bee973bfce3cfedb64dd7708b1c6f51a8fb14
squirrelnest <mikkasoft@gmail.com>
2024-07-22 14:11:15 -0400

    format output and identify dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -4,15 +4,16 @@ import json
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = { "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock" }
-    json = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json, headers)
+    input = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = input, headers = headers).text
     response_dict = json.loads(response)
+    predictions = response_dict["emotionPredictions"][0]["emotion"]
     emotions = {}
     
-    for key, value in response_dict.emotionPredictions[0].emotion.items():
+    for key, value in predictions.items():
         emotions[key] = value
-        if "dominant_emotion" in emotions
-            if value > emotions["dominant_emotion"]:
+        if "dominant_emotion" in emotions:
+            if value > predictions[emotions["dominant_emotion"]]:
                 emotions["dominant_emotion"] = key
         else:
             emotions["dominant_emotion"] = key
